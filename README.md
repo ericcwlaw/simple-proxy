d63d8c1bce662bb1686cae4cdecc0ccb5128a6af
Eric Law <eric.cw.law@gmail.com>
2020-09-12 15:03:55 -0700

    v1.0.0

---
+++ README.md
@@ -0,0 +1,51 @@
+# Simple TCP socket proxy
+
+This proxy was originally designed to allow me to ssh into a Ubuntu multipass VM from the host machine or the Internet.
+
+I am amazed about the simplicity and performance of the proxy, thus polishing it to become a generic socket proxy that you can use to do network address translation (NAT) from a host port to a guest VM target IP and port.
+
+## proxy-config.json
+
+The IP discovery command, source and target ports are configurable using a JSON file like this. You may adjust the configuration file to fit your use case.
+
+```json
+{
+  "discovery": {
+    "command": "multipass exec main ifconfig eth0",
+    "tag": "inet",
+    "index": 1
+  },
+  "source_port": 22,
+  "target_port": 22
+}
+```
+
+## SSH security
+
+If you use this to expose your guest VM to the Internet, make sure you use certificate authentication and disable password authentication. You should also use SSH certificate of 4,096 bits for security reason.
+
+If you are using Multipass, you can replace the original SSH certificate with a 4,096-bit certificate.
+
+## Running this utility as a service
+
+We recommend that you use Node's PM2 process manager to deploy the utility as a service.
+
+It is as simple as:
+
+```
+pm2 start simple-proxy.js
+```
+This assumes you start the utility from the project directory. If not, please add path to the filename.
+
+## Library dependencies
+
+Please install the moment time utility before you start the utility.
+
+```
+cd simple-proxy
+npm install moment
+```
+
+Hope you enjoy this utility for your projects.
+
+9/12/2020
---
+++ simple-proxy.js
@@ -0,0 +1,163 @@
+const Shell = require('child_process');
+const DateFormat = require('moment');
+const Net = require('net');
+const Crypto = require('crypto');
+const fs = require('fs');
+const NumberFormat = new Intl.NumberFormat();
+
+const APP_NAME = 'Simple Proxy v1.0.0';
+const TIMESTAMP = 'YYYY-MM-DD HH:mm:ss.SSS';
+const ZEROES = '000000';
+const instanceId = (ZEROES + Crypto.randomBytes(4).readUIntBE(0, 4) % 10000).slice(-4);
+
+const consoleLog = (message) => {
+    console.log(DateFormat().format(TIMESTAMP)+' ['+instanceId+'] '+message);
+};
+
+async function portReady(host, port) {
+    return new Promise((resolve, reject) => {
+        const client = Net.createConnection({ port: port, host: host }, () => {
+            client.end();
+            resolve(true);
+        });
+        client.on('error', function(err) {
+            client.end();
+            resolve(false);
+        });
+    });
+};
+
+async function getVmIpAddress(command, key, index) {
+    return new Promise((resolve, reject) => {
+        Shell.exec(command, (error, stdout, stderr) => {
+            if (error) {
+                consoleLog(error.message.split('\n')[0]);
+            } 
+            if (stdout) {
+                var ip = stdout.split('\n').filter(v => v.toString().trim().startsWith(key+' '))[0].trim().split(' ').filter(v => v.length > 0)[index];
+                resolve([true, ip]);
+            } else {
+                resolve([false, stderr.split('\n')[0]]);
+            }
+        });
+    });
+}
+
+const forwardPort = (sourcePort, ip, targetPort) => {
+    const connections = new Map();
+    // Setup TCP socket server
+    const server = new Net.Server();
+    server.listen(sourcePort, '0.0.0.0', () => {
+        consoleLog('Forwarding port-'+sourcePort+' to '+ip+':'+targetPort);
+    });
+    // Graceful shutdown
+    const gracefulShutdown = () => {
+        for (const k of connections.keys()) {
+            consoleLog('Stopping '+k);
+            connections.get(k).end();
+        }
+        server.close(() => {
+            consoleLog('Proxy service stopped');
+        });
+    };
+    process.on('SIGTERM', () => {
+        consoleLog('Kill signal detected');
+        gracefulShutdown();
+    });
+    process.on('SIGINT', () => {
+        consoleLog('Control-C detected');
+        gracefulShutdown();
+    });
+    // Start TCP socket server
+    server.on('connection', (socket) => {
+        var normal = true;
+        const sessionId = (ZEROES + Crypto.randomBytes(4).readUIntBE(0, 4) % 1000000).slice(-6);
+        const client = Net.createConnection({ port: targetPort, host: ip }, () => {
+            connections.set(sessionId, client);
+            consoleLog( 'Session ' + sessionId + ' connected to ' + socket.remoteAddress +
+                        ' sessions=' + connections.size );
+        });
+        socket.on('data', (data) => {
+            if (normal) client.write(data);
+        });
+        socket.once('end', () => {
+            consoleLog('Session '+sessionId+' closed by '+socket.remoteAddress);
+            client.end();
+        });
+        client.on('data', (data) => {
+            if (normal) socket.write(data);
+        });
+        client.once('end', () => {
+            socket.end();
+            connections.delete(sessionId);
+            consoleLog( 'Session '+sessionId+' disconnected from ' + socket.remoteAddress +
+                        ' rx=' + NumberFormat.format(socket.bytesRead) +
+                        ' tx=' + NumberFormat.format(socket.bytesWritten) + ' sessions='+connections.size );
+        });
+        // Socket exceptions - most likely to be read timeout or connection reset
+        socket.on('error', (err) => {
+            normal = false;
+            client.end();
+            socket.end();
+            if ('ECONNRESET' == err.code) {
+                // normal case when user is using Windows
+                consoleLog('Session '+sessionId+' closed by '+socket.remoteAddress);
+            } else {
+                consoleLog('Exception ('+socket.remoteAddress+') - '+err.code);
+            }
+        });
+        client.on('error', (err) => {
+            normal = false;
+            socket.end();
+            client.end();
+            // Caution: the error message may depend on locale and language
+            if (err.message.startsWith('connect ETIMEDOUT')) {
+                // Let process manager to restart this app
+                consoleLog('Stopping application because target does not respond');
+                process.exit(1);
+            } else {
+                consoleLog('Exception ('+ip+') - '+err);
+            }
+        });
+    });
+}
+
+async function main() {
+    consoleLog(APP_NAME);
+    if (fs.existsSync('proxy-config.json')) {
+        const fd = fs.openSync('proxy-config.json');
+        var text = fs.readFileSync(fd, 'utf-8');
+        fs.closeSync(fd);
+        var json = JSON.parse(text);
+        var command = json['discovery']['command'];
+        var tag = json['discovery']['tag'];
+        var index = json['discovery']['index'];
+        var source_port = json['source_port'];
+        var target_port = json['target_port'];
+        if (command && tag && index && source_port && target_port) {
+            if (await portReady('127.0.0.1', source_port)) {
+                consoleLog('Port-'+source_port+' is already used');
+            } else {
+                consoleLog('Port-'+source_port+' is available');
+                // Obtain dynamic IP address - this assumes we are using multipass and the VM is called "main"
+                const [valid, result] = await getVmIpAddress(command, tag, index);
+                if (valid) {
+                    if (await portReady(result, target_port)) {
+                        forwardPort(source_port, result, target_port);
+                    } else {
+                        consoleLog('Target '+result+':'+target_port+' does not respond');
+                    }
+                } else {
+                    consoleLog('Unable to obtain target IP address - '+result);
+                }
+            }
+        } else {
+            consoleLog('Invalid proxy-config.json');
+        }
+    } else {
+        consoleLog('Missing proxy-config.json');
+    }
+}
+
+// start main application
+main();
